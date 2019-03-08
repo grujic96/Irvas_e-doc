586e8936f64bcc708360d71639fdde76472b5719
grujic96 <aleksandar.grujicc@gmail.com>
2019-03-08 16:16:39 +0100

    [imp]-izmenjen form view za prijem i otpremu kreirani novi tree view, dodate stavke meni-a

--- irvas_muk_dms_extension/models/irvas_muk_dms_ext.py
+++ irvas_muk_dms_extension/models/irvas_muk_dms_ext.py
@@ -6,16 +6,27 @@ from odoo import models, api, fields
 class MukDmsExt(models.Model):
     _inherit = "muk_dms.file"
 
-
     object = fields.Text(string="Predmet")
     sub_number = fields.Integer(string="Pod Broj",store=True, compute='number_onchange')
     create_date = fields.Datetime(string="Datum")
     contact_id = fields.Many2one('res.partner',string="Posiljaoc")
     document_class_id = fields.Many2one('irvas.edoc.document.class', string="Klasa Dokumenta")
+    date_receive = fields.Datetime(string="Datum Prijema")
+    date_shipped = fields.Datetime(string="Datum Otpreme")
+
+    digital_signature = fields.Binary(string="Digital Signature")
+    #received_type = fields.Selection([('on_hands', 'On Booking'),
+     #                                ('manual', 'On Check In'),
+      #                               ('picking', 'On Checkout')], description='The way document was received', string="Receive Type")
+
+
+    def _set_document_type(self):
+        self.document_type = self._context.get('document_type')
+
+    document_type = fields.Selection([('prijem','Prijem'), ('otprema','Otprema')], default=_set_document_type, store=True, readonly=True)
 
 
     def _get_number_id(self):
-        print('usoooo')
         file = self.env['muk_dms.file'].search([])
         a=[]
         max_number= 0
@@ -24,8 +35,6 @@ class MukDmsExt(models.Model):
         for i in range(len(a)):
             if a[i]> max_number:
                 max_number = a[i]
-            print(a[i])
-        print(max_number)
         if max_number == 0:
             return 1
         else:
@@ -46,3 +55,57 @@ class MukDmsExt(models.Model):
                 if f.sub_number > max_subnumber:
                     max_subnumber = f.sub_number
             self.sub_number = max_subnumber + 1
+
+
+    def open_create_document_received(self,ctx, context=None):
+        context=ctx
+        print(context)
+        print(context.get('izlazni'))
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'muk_dms.file',
+            'view_mode': 'form',
+            'target': 'current',
+            'context': context,
+            'flags': {'form': {'action_buttons': True, 'options': {'mode': 'create'}}}
+        }
+
+    def open_create_document_shipping(self,ctx, context=None):
+        context=ctx
+
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'muk_dms.file',
+            'view_mode': 'form',
+            'target': 'current',
+            'context': context,
+            'flags': {'form': {'action_buttons': True, 'options': {'mode': 'create'}}}
+        }
+
+    # @api.onchange('location_dest_id')
+    # def get_document_type(self, context=None):
+    #     if self.location_dest_id:
+    #         res = {}
+    #         micro_list = []
+    #
+    #         if 'sit_prelociranje_form_action' in self._context:
+    #             micro_location = self.env['stock.microlocation'].search(
+    #                 [('stock_location_id', '=', self.location_dest_id.id)], limit=1)
+    #             # self.micro_loc_dest_id = micro_location.id
+    #             micro_location_obj = self.env['stock.microlocation']
+    #             micro_location_ids = micro_location_obj.search([('stock_location_id', '=', self.location_dest_id.id)])
+    #             for record in micro_location_ids:
+    #                 micro_list.append(record.id)
+    #             res = {'domain': {'micro_loc_dest_id': [('id', 'in', micro_list)]}}
+    #             return res
+    #
+    #         if 'sit_trebovanje_form_action' in self._context:
+    #             micro_location_obj = self.env['stock.microlocation']
+    #             micro_location_ids = micro_location_obj.search([[u'stock_location_id.name', u'ilike', u'SI u upotrebi'],
+    #                                                             ('stock_location_id.usage', '=', 'asset')])
+    #             for record in micro_location_ids:
+    #                 micro_list.append(record.id)
+    #             res = {'domain': {'micro_loc_dest_id': [('id', 'in', micro_list)]}}
+
+
+
